80fde377223cdcef51ee44b87e0e77357527d262
Gaurav <Gauravsb105@gmail.com>
2025-06-22 20:23:05 +0530

    added lcoation i the model

--- src/Component/EventModel/page.jsx
+++ src/Component/EventModel/page.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useRef, useState } from "react";
+import { MapPin } from "lucide-react";
 import imageToDispaly from "../../assets/DSEULogo/BANNER-DESIGN-7.jpg";
+
+const location = 'https://www.google.com/maps/place/DSEU+Wazirpur-I+Campus+%26+DSEU+Okhla-II+Campus/@28.7007126,77.165031,270m/data=!3m1!1e3!4m6!3m5!1s0x390d022484bf6f49:0xe9aeece4d5bbbbc8!8m2!3d28.700841!4d77.1649905!16s%2Fm%2F0115kn0c?entry=ttu&g_ep=EgoyMDI1MDYxNy4wIKXMDSoASAFQAw%3D%3D';
 
 const Page = () => {
   const [isVisible, setIsVisible] = useState(true);
@@ -36,11 +39,26 @@ const Page = () => {
         >
           &times;
         </button>
-        <img
-          src={imageToDispaly}
-          alt="Event"
-          className="rounded w-[350px] sm:w-[340px] md:w-[420px] lg:w-[500px]"
-        />
+
+        {/* Image wrapper to position icon over it */}
+        <div className="relative inline-block">
+          <img
+            src={imageToDispaly}
+            alt="Event"
+            className="rounded w-[280px] sm:w-[340px] md:w-[420px] lg:w-[500px]"
+          />
+
+          {/* Location icon overlay (bottom-right corner) */}
+          <a
+            href={location}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="absolute bottom-2 right-2 bg-white bg-opacity-70 hover:bg-opacity-90 rounded-full p-1"
+            title="View Location on Map"
+          >
+            <MapPin className="text-red-600 w-6 h-6" />
+          </a>
+        </div>
       </div>
     </div>
   );
