5871853b07b87d0782ffb1293512bf66ee385675
Gaurav <gauravsb105@gmail.com>
2025-07-13 22:20:55 +0530

    name changes

--- src/Component/Grievance/rtiData.js
+++ src/Component/Grievance/rtiData.js
@@ -216,7 +216,7 @@ export const rtiData = {
       mail:"rti@dseu.ac.in"
     },
     {
-      title: "Dr. Gagan Dhawan",
+      title: "Prof. Gagan Dhawan",
       designation: "First Appellate Authority",
       mail:"rti@dseu.ac.in"
     }
